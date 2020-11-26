28d55661997bedd7dc282411ce1a0b0d32b3310f
Luiz Mayer <6317@htl.rennweg.at>
2020-11-26 11:17:44 +0100

    Finalize project

--- migrations/Version20201126100949.php
+++ migrations/Version20201126100949.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200921084853 extends AbstractMigration
+final class Version20201126100949 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,7 @@ final class Version20200921084853 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE tournament_entry (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, traveldistance DOUBLE PRECISION NOT NULL)');
+        $this->addSql('CREATE TABLE tournament_entry (id INT AUTO_INCREMENT NOT NULL, traveldistance DOUBLE PRECISION NOT NULL, planemodel VARCHAR(255) NOT NULL, flightduration DOUBLE PRECISION DEFAULT NULL, participant VARCHAR(255) NOT NULL, date DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
